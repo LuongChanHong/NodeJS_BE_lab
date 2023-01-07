2937d764ab95e67cdb4f7f61096ea903a30d8e68
honglc <hongmainmail@gmail.com>
2023-01-07 18:45:35 +0700

    lab 17.2 add form validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,12 +2,13 @@ const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer"); // node mail handler
 const sendgridTransport = require("nodemailer-sendgrid-transport"); // sendgrid free mail server
 const mongodb = require("mongodb");
+const { validationResult } = require("express-validator");
 
 const User = require("../models/User");
 const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
-      api_user: "hongmainmail@gmail.com",
+      api_user: "funix-nodejs-lab",
       api_key:
         "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", // key from sendgrid (page) user API Key fearture
     },
@@ -20,24 +21,24 @@ const comparePassword = async (password, hashPassword) => {
 
 exports.login = async (req, res) => {
   const reqData = req.body.data;
-  // console.log(reqData);
-
-  const loginUser = await User.findOne({ email: reqData.email });
-  // console.log("loginUser:", loginUser);
-  // console.log("isMatch:", isMatch);
-  if (loginUser !== null) {
+  console.log(reqData);
+  const errors = validationResult(req).array({ onlyFirstError: true });
+  // console.log("errors:", errors);
+  if (errors.length <= 0) {
+    const loginUser = await User.findOne({ email: reqData.email });
     const isMatch = await comparePassword(reqData.password, loginUser.password);
+    // console.log("isMatch:", isMatch);
     if (isMatch) {
       req.session.isLoggedIn = true;
       res.send(req.session.isLoggedIn);
     } else {
-      res.statusMessage = "Password wrong";
-      res.status(404).end();
+      errors.push({ msg: "Wrong password" });
+      res.send(errors);
     }
   } else {
-    res.statusMessage = "Email wrong";
-    res.status(404).end();
+    res.send(errors);
   }
+  res.end();
 };
 
 exports.logout = (req, res) => {
@@ -52,18 +53,26 @@ exports.logout = (req, res) => {
 exports.signup = async (req, res) => {
   const reqData = req.body;
   const hashPass = await bcrypt.hash(reqData.password, 12);
-  const newUser = new User({
-    email: reqData.email,
-    password: hashPass,
-    cart: { items: [] },
-  });
-  newUser.save();
+  const errors = validationResult(req);
+  console.log("errors:", errors);
+  if (errors.length <= 0) {
+    const newUser = new User({
+      email: reqData.email,
+      password: hashPass,
+      cart: { items: [] },
+    });
+    // newUser.save();
+  } else {
+    res.send(errors);
+  }
 
-  await transporter.sendMail({
-    to: reqData.email,
-    from: "mail@gmail.com",
-    subject: "Signup Success",
-    html: "<h1>Signup Success</h1>",
-  });
+  // transporter
+  //   .sendMail({
+  //     to: reqData.email,
+  //     from: "mail@gmail.com",
+  //     subject: "Signup Success",
+  //     html: "<h1>Signup Success</h1>",
+  //   })
+  //   .then((res) => console.log("res:", res));
   res.end();
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,56 @@
 const express = require("express");
 const router = express.Router();
+const { check, body } = require("express-validator");
 
+const User = require("../models/User");
 const authController = require("../controllers/auth");
 
-router.post("/login", authController.login);
+router.post(
+  "/login",
+  [
+    body("data.email")
+      .isEmail()
+      .withMessage("Email invaliad")
+      .custom(async (value) => {
+        // custom async valida with custom err mess
+        return await User.findOne({ email: value }).then((user) => {
+          if (!user) {
+            return Promise.reject("This email don't exist, try another one");
+          }
+        });
+      }),
+  ],
+  authController.login
+);
 router.post("/logout", authController.logout);
-router.post("/signup", authController.signup);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Email invaliad") // custom error mess
+      .custom(async (value) => {
+        // custom async valida with custom err mess
+        return await User.findOne({ email: value }).then((user) => {
+          if (user) {
+            return Promise.reject("This email exist, try another one");
+          }
+        });
+      }),
+    //   body("password", "Password can't empty and must more than 8 character") // custom err mess for all validation item link
+    body("password", "Password must more than 8 character") // custom err mess for all validation item link
+      .isLength({ min: 8 }),
+    body("confirm")
+      .isEmpty()
+      .custom((value, { req }) => {
+        // custom valida with custom err mess
+        if (value != req.body.password) {
+          throw new Error("Confirm not match Password");
+        }
+        return true;
+      }),
+  ],
+  authController.signup
+);
 
 exports.route = router;
